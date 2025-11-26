3dcf7602f7e225fa28b47e0d416e1d874b575b45
Luca Fazio <yougotricked2@gmail.com>
2025-11-25 23:47:09 -0800

    Added possible Unit tests for when i add funcs

---
+++ cpp/harness_subex.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int add(int, int);
+
+int main() {
+    struct Case { int a, b, expected; } cases[] = {
+        {1, 2, 3},
+        {-1, 5, 4},
+        {0, 0, 0},
+        {1000, -1000, 0},
+    };
+
+    bool all_ok = true;
+    for (auto c : cases) {
+        int got = add(c.a, c.b);
+        if (got != c.expected) {
+            std::cout << "[FAIL] add(" << c.a << ", " << c.b
+                      << ") expected " << c.expected
+                      << " got " << got << "\\n";
+            all_ok = false;
+        }
+    }
+
+    if (all_ok) {
+        std::cout << "[PASS]\n";
+        return 0;
+    }
+    return 1;
+}
