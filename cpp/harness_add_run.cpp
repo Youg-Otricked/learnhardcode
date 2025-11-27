59ea4f4d8653c10bdd78a38e9fa56915b81d5a6c
Luca Fazio <yougotricked2@gmail.com>
2025-11-26 17:14:01 -0800

    Finished first unit tests and lesson20

--- cpp/harness_add_run.cpp
+++ cpp/harness_add_run.cpp
@@ -7,6 +7,6 @@ int main() {
         std::cout << "[FAIL] add(1, 2) should be 3\n";
         return 1;
     }
-    std::cout << "[PASS]\n";
+    std::cout << "--------------------------\n[PASS](1/1)\n(1/1): 1+2 = 3\n";
     return 0;
 }
