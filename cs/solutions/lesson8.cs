cd2a8e878b98a45ebc1f980da3312bde10adaa3c
Luca Fazio <yougotricked2@gmail.com>
2025-12-16 19:03:24 -0800

    8

---
+++ cs/solutions/lesson8.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Chronoshard.core
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            string PlayerName = "Name";
+            string GameName = "ChronoShard";
+
+            Console.WriteLine($"Player {PlayerName} has logged into {GameName}.");
+            return 0;
+        }
+    }
+}
