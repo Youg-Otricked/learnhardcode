387082bed9938607a436030c3eee857740dc3e61
Luca Fazio <yougotricked2@gmail.com>
2025-12-16 19:03:24 -0800

    8

--- cs/CreateLesson.sh
+++ cs/CreateLesson.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 n="$1"
 next=$((n + 1))
 prev=$((n - 1))
@@ -5,13 +7,11 @@ prev=$((n - 1))
 mkdir -p lessons
 
 content="$CSHARP_LESSON_TEMPLATE"
-
-# Replace id
 content="${content//lessonX.json/lesson$n.json}"
-
-# Replace nextLesson and previous
 content="${content//lessonNEXT.json/lesson$next.json}"
 content="${content//lessonPREV.json/lesson$prev.json}"
-echo $content > lessons/lesson$1.json
-jq . "lessons/lesson$1.json" > "lessons/lesson$1.pretty.json"
-mv "lessons/lesson$1.pretty.json" "lessons/lesson$1.json"
+
+# Write content exactly as-is, then pretty-print with jq
+printf "%s\n" "$content" > "lessons/lesson$n.raw.json"
+jq . "lessons/lesson$n.raw.json" > "lessons/lesson$n.json"
+rm "lessons/lesson$n.raw.json"
