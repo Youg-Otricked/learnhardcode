f67c5e9ba171bcc1b7db6a770c0f920076e0aa6a
Luca Fazio <yougotricked2@gmail.com>
2026-03-24 21:08:10 -0700

    Way better UI

---
+++ cpp/chapters.js
@@ -0,0 +1,22 @@
+let params = new URLSearchParams(location.search);
+let file = `${params.get('course') ? params.get('course') + "_" : ""}chapters.json`;
+const chapters = await (await fetch(file)).json();
+const completedLessons = JSON.parse(localStorage.getItem("cpp_completed_lessons") || '[]');
+console.log(chapters);
+const completedSet = new Set(Object.keys(completedLessons));
+const chaptersElem = document.getElementById("chapters");
+//<details id="chapters" style="position: absolute; top: 0; right: 0; text-align: center;"><summary style="padding: 0 30px;">Chapters</summary><button style="background: none; padding: 0; border: none; cursor: pointer; margin: 0 auto; font-size: 19;">◉ Variables</button></details>
+for (const chapter of chapters["chapters"].sort((a, b) => a.number - b.number)) {
+    const btn = document.createElement("button");
+    btn.style.cssText = "background: none; padding: 0; border: none; cursor: pointer; margin: 0 auto; font-size: 19px;";
+    const br = document.createElement("br");
+    const allCompleted = chapter.lessons.every((l) => completedSet.has(l));
+    btn.textContent = `${allCompleted ? '◉' : '◯'} ${chapter.name}`;
+    btn.onclick = () => {
+        window.location.href = `?lesson=${chapter.firstLesson}`;
+    };
+    chaptersElem.appendChild(btn);
+    chaptersElem.appendChild(br);
+}
+export {};
+//# sourceMappingURL=uitest.js.map
---
+++ cs/chapters.js
@@ -0,0 +1,20 @@
+const chapters = await (await fetch("./chapters.json")).json();
+const completedLessons = JSON.parse(localStorage.getItem("cs_completed_lessons") || '[]');
+console.log(chapters);
+const completedSet = new Set(Object.keys(completedLessons));
+const chaptersElem = document.getElementById("chapters");
+//<details id="chapters" style="position: absolute; top: 0; right: 0; text-align: center;"><summary style="padding: 0 30px;">Chapters</summary><button style="background: none; padding: 0; border: none; cursor: pointer; margin: 0 auto; font-size: 19;">◉ Variables</button></details>
+for (const chapter of chapters["chapters"].sort((a, b) => a.number - b.number)) {
+    const btn = document.createElement("button");
+    btn.style.cssText = "background: none; padding: 0; border: none; cursor: pointer; margin: 0 auto; font-size: 19px;";
+    const br = document.createElement("br");
+    const allCompleted = chapter.lessons.every((l) => completedSet.has(l));
+    btn.textContent = `${allCompleted ? '◉' : '◯'} ${chapter.name}`;
+    btn.onclick = () => {
+        window.location.href = `?lesson=${chapter.firstLesson}`;
+    };
+    chaptersElem.appendChild(btn);
+    chaptersElem.appendChild(br);
+}
+export {};
+//# sourceMappingURL=uitest.js.map
