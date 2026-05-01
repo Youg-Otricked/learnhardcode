9c30edac57d9a3c65f3d31eb3bcdb9ca8f3d0267
Youg-Otricked <yougotricked2@gmail.com>
2026-04-30 19:28:02 -0700

    fix packs.mjs path

--- cpp/packs.mjs
+++ cpp/packs.mjs
@@ -1,6 +1,6 @@
 // No imports at the top!
 export default {
     "cpython": "./packages/cpython.pack",
-    "emscripten": "./packages/emscripten.pack",
+    "emscripten": "https://github.com/Youg-Otricked/learnhardcode/releases/download/1.0.0/emscripten.pack",
     "wasm": "./packages/wasm.pack"
 };
