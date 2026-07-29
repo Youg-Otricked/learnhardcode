8112261a8fe5c5a722474b3be4e22b0885df4977
Youg-Otricked <yougotricked2@gmail.com>
2026-07-29 10:00:19 -0700

    Lesson stdlib44. Working on more git soon

--- lesson.md
+++ lesson.md
@@ -1,20 +1,26 @@
-Now, we have used list to see all your config values. Get gets one specific value.
-Think of list like
-```bash
-cat $(ls .)
+A stack is a FILO (First in Last Out) collection. It's called a stack because it behaves like a stack of objects.
+```psuedocode
+# psuedocode
+stack<int> mystack = [1, 2, 3, 4]
+mystack.push(3)
+# mystack is now [1, 2, 3, 4, 3]
+int res = mystack.pop()
+# res = 3
+# mystack = [1, 2, 3, 4]
 ```
-and get like
-```bash
-cat <thing>
-```
-```git
-git config get <key>
-```
-Keys are still in that format from a few lessons ago, <namespace>.<key>
 
----
 ## Assignment:
+Implement a basic stack!
+Stacks are much easier than the other datastructures, becuase they don't even need you to make indexing.
 
-Our lone investor wants to see how epically we wasted his money!
-Use `get` to get the `getapp.users` key from your local config.
+### Steps:
 
+
+- [ ] Make the "push" method
+    - [ ] Add the element to the vector storing current data
+- [ ] Make the "top" method
+    - [ ] Return a _reference_ to the last element
+- [ ] Make the "size" method
+    - [ ] Return the amount of elements in the stack
+- [ ] Make the "pop" method
+    - [ ] Remove the final element of the internal vector and return it
--- test.cpp
+++ test.cpp
@@ -1,41 +1,3 @@
-#include <iostream>
-#include <initializer_list>
-#include <algorithm>
-template <typename T, int S = 1>
-class FSizeArr {
-private:
-    T* value;
-public:
-    int length;
-    FSizeArr(std::initializer_list<T> init) {
-        this->length = S > 1 ? S : 1;
-        this->value = new T[this->length];
-        std::copy(init.begin(), init.end(), this->value);
-    }
-    FSizeArr() {
-        this->length = S > 1 ? S : 1;
-        this->value = new T[this->length];
-    }
-    ~FSizeArr() {
-        delete[] this->value;
-        this->value = nullptr;
-    }
-    T& operator[](size_t index) {
-        return this->value[index];
-    }
-    const T& operator[](size_t index) const {
-        return this->value[index];
-    }
-    const T back() const {
-        return this->value[this->length - 1];
-    }
-    const T front() const {
-        return this->value[0];
-    }
-    const T* data() const {
-        return this->value;
-    }
-};
 int _pass = 0;
 int _fail = 0;
 void _test(int num, int got, int expected) {
@@ -47,10 +9,15 @@ void _test(int num, int got, int expected) {
 }
 
 int main() {
-    FSizeArr<int, 3> tester1 = {1, 2, 3};
-    tester1.~FSizeArr();
-    _test(1, 1, tester1.data() == nullptr ? 1 : 0);
+    Stack<int> tester1 = {1, 2, 3};
+    tester1.push(91);
+    _test(1, 91, tester1.top());
+    tester1.top() = 12;
+    _test(1, 12, tester1.top());
+    _test(1, 12, tester1.pop());
+    _test(1, 3, tester1.top());
     std::cout << "---------------------\n";
     std::cout << _pass << " passed, " << _fail << " failed\n";
     return 0;
 }
+
